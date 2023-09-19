37058769ab095321da73c1c5797a0b543c506b7c
Daniel Watanabe <92555368+Daniel-Wata@users.noreply.github.com>
2023-09-18 21:48:46 -0300

    FEAT added product and category

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import Head from 'next/head'
-import { Header } from "../../components/ui/Header"
+import { Header } from "../../components/Header"
 
 export default function Dashboard(){
     return(
